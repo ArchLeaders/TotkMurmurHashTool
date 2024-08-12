a937dfbc5985e7fe8e4164ab6ea93fa7850a5deb
ArchLeaders <archleadership28@gmail.com>
2024-08-12 02:39:22 -0700

    Impl command line interface

---
+++ src/TotkMurmurHashTool/Helpers/ArgumentParser.cs
@@ -0,0 +1,20 @@
+﻿using FluentAvalonia.Core;
+
+namespace TotkMurmurHashTool.Helpers;
+
+public static class ArgumentParser
+{
+    public static void GetInputs(this string[] args, out string[] actorNames, out string[] constants)
+    {
+        int constantsMark = args.IndexOf("-c");
+
+        if (constantsMark <= -1) {
+            actorNames = args;
+            constants = [];
+            return;
+        }
+
+        actorNames = args[..constantsMark];
+        constants = args[++constantsMark..];
+    }
+}
--- src/TotkMurmurHashTool/Program.cs
+++ src/TotkMurmurHashTool/Program.cs
@@ -1,16 +1,28 @@
 ﻿using Avalonia;
+using TotkMurmurHashTool.Helpers;
 
 namespace TotkMurmurHashTool;
 
 class Program
 {
     [STAThread]
-    public static void Main(string[] args) => BuildAvaloniaApp()
-        .StartWithClassicDesktopLifetime(args);
+    public static void Main(string[] args)
+    {
+        if (args.Length > 0) {
+            args.GetInputs(out string[] actorNames, out string[] constants);
+            Console.WriteLine(YamlResultHelper.CreateGdlResult(actorNames, constants));
+            return;
+        }
+
+        BuildAvaloniaApp()
+            .StartWithClassicDesktopLifetime(args);
+    }
 
     public static AppBuilder BuildAvaloniaApp()
-        => AppBuilder.Configure<App>()
+    {
+        return AppBuilder.Configure<App>()
             .UsePlatformDetect()
             .WithInterFont()
             .LogToTrace();
+    }
 }
